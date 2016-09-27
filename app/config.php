3308544150cc412a93d993fd383b5d00528ae481
Sébastien Lucas <sebastien@slucas.fr>
2016-09-27 15:37:38 +0200

    Update copyright

--- app/config.php
+++ app/config.php
@@ -1,4 +1,15 @@
 <?php
+/**
+ * This file is part of SAF Photo Gallery
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @copyright  Copyright(c) 2016 Sébastien Lucas
+ * @author     Sébastien Lucas <sebastien@slucas.fr>
+ */
+
 // Config that gets passed into main Silex/App instance
 return array(
     'env' => 'development',
--- web/index.php
+++ web/index.php
@@ -1,4 +1,15 @@
 <?php
+/**
+ * This file is part of SAF Photo Gallery
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @copyright  Copyright(c) 2016 Sébastien Lucas
+ * @author     Sébastien Lucas <sebastien@slucas.fr>
+ */
+
 define('SILEX_ROOT', dirname(__FILE__) . '/../');
 define('SILEX_APP_ROOT', SILEX_ROOT . '/app/');
 define('SILEX_SRC_ROOT', SILEX_APP_ROOT . '/src/');
